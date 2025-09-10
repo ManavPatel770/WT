8280bad3e2a2d9f2eaa1a2ce5f59dc6da3194ffc
manav <mkpatel8571@gmail.com>
2025-09-10 22:17:58 +0530

    Updated Demo JSX

--- Lab-2/Demo.jsx
+++ Lab-2/Demo.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 export const Demo = () => {
+    const abc = "Patel manav"
   return (
-    <div>Demo</div>
+    <div>
+    <h1>My name manu</h1>
+    <h1>{abc}</h1>
+    </div>
   )
 }
